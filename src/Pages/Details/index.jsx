ab0f3a4d05723905c177951161a6882f4ffb1b3d
Dante <gustavobarboza2003@gmail.com>
2024-03-05 15:43:41 -0300

    Initial commit, starting study in 'styled-components' and 'React'

---
+++ src/Pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Details() {
+  return (
+    <Container>
+      <Button title="Voltar" />
+    </Container>
+  );
+}
