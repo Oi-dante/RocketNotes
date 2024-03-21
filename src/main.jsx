7058ad5c7d6d2abe5e2b0e57ab6386547e39e658
Dante <gustavobarboza2003@gmail.com>
2024-03-21 14:55:26 -0300

    Creation of registration and login pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./Pages/Home";
 import { ThemeProvider } from "styled-components";
 
-import GlobalStyles from './Styles/global'
+import GlobalStyles from "./Styles/global";
 import theme from "./Styles/theme";
+
+import { SignUp } from "./Pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
